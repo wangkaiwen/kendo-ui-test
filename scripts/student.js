e0185e456f40bf2b51c95c0bdfa7cb347b7b612f
kevin <876730203@qq.com>
2016-12-02 18:01:24 +0800

    Create student.js

---
+++ scripts/student.js
@@ -0,0 +1,127 @@
+var meeting=[{
+    time:{
+        date:"04",
+        weekday:"星期五"
+    },
+    message:{
+        title:"图片",
+        more:{
+            host:"XXX",
+            meeting_time:"2016-03-04 14:00",
+            place:"粉色粉色很听话",
+            person:"管理员",
+            release_time:"2016-03-04 14:10:08",
+            update_time:"2016-03-04 14:30:08"
+        }
+    },
+    code:"1561234",
+    status:"已发布"
+},{
+    time:{
+        date:"04",
+        weekday:"星期五"
+    },
+    message:{
+        title:"图片",
+        more:{
+            host:"XXX",
+            meeting_time:"2016-03-04 14:00",
+            place:"法法网粉色发色粉",
+            person:"管理员",
+            release_time:"2016-03-04 14:10:08",
+            update_time:"2016-03-04 14:30:08"
+        }
+    },
+    code:"1561234",
+    status:"已发布"
+},{
+    time:{
+        date:"04",
+        weekday:"星期五"
+    },
+    message:{
+        title:"图片",
+        more:{
+            host:"特提供的",
+            meeting_time:"2016-03-04 14:00",
+            place:"回复回复eternal",
+            person:"管理员",
+            release_time:"2016-03-04 14:10:08",
+            update_time:"2016-03-04 14:30:08"
+        }
+    },
+    code:"1561234",
+    status:"已发布"
+},{
+    time:{
+        date:"04",
+        weekday:"星期五"
+    },
+    message:{
+        title:"图片",
+        more:{
+            host:"XXX",
+            meeting_time:"2016-03-04 14:00",
+            place:"XX",
+            person:"管理员",
+            release_time:"2016-03-04 14:10:08",
+            update_time:"2016-03-04 14:30:08"
+        }
+    },
+    code:"1561234",
+    status:"已发布"
+},{
+    time:{
+        date:"04",
+        weekday:"星期五"
+    },
+    message:{
+        title:"图片",
+        more:{
+            host:"XXX",
+            meeting_time:"2016-03-04 14:00",
+            place:"还几个镜头以及",
+            person:"管理员",
+            release_time:"2016-03-04 14:10:08",
+            update_time:"2016-03-04 14:30:08"
+        }
+    },
+    code:"1561234",
+    status:"已发布"
+},{
+    time:{
+        date:"04",
+        weekday:"星期五"
+    },
+    message:{
+        title:"图片",
+        more:{
+            host:"XXX",
+            meeting_time:"2016-03-04 14:00",
+            place:"XX",
+            person:"管理员",
+            release_time:"2016-03-04 14:10:08",
+            update_time:"2016-03-04 14:30:08"
+        }
+    },
+    code:"1561234",
+    status:"已发布"
+},{
+    time:{
+        date:"04",
+        weekday:"星期五"
+    },
+    message:{
+        title:"图片",
+        more:{
+            host:"XXX",
+            meeting_time:"2016-03-04 14:00",
+            place:"XX",
+            person:"管理员",
+            release_time:"2016-03-04 14:10:08",
+            update_time:"2016-03-04 14:30:08"
+        }
+    },
+    code:"1561234",
+    status:"已发布"
+}];
